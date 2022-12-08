3698e31bc20d3ae1e454b02e9816a81d0639dede
kaushikpaul <kaushik.paul755@gmail.com>
2022-12-08 19:21:35 +0530

    Initial commit

--- src/decorators.ts
+++ src/decorators.ts
@@ -3,6 +3,8 @@ function Component(constructor: Function) {
     constructor.prototype.uniqueId = Date.now()
     constructor.prototype.insertInDOM = () => { 
         console.log("Inserting the component in the DOM");
+        console.log("Hello there");
+        
     }
 }
 
